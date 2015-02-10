136b92d57f180f44e7ea1b14101b6ea04324270b
Shaba Abhiram <shabarivas.abhiram@gmail.com>
2015-02-09 16:23:54 -0800

    adds command line flags and app versioning

---
+++ wol/version.go
@@ -0,0 +1,56 @@
+// This file contains version specific, and usage information for
+// the wol application
+package main
+
+import (
+    "fmt"
+    "github.com/sabhiram/colorize"
+)
+
+// Version represents the current Semantic Version of this application
+const Version = "1.0.0"
+
+// List of options which chloe supports
+var ValidOptions = [] struct {
+    short, long, description string
+} {
+    { `v`, `version`, `prints the application version`                    },
+    { `h`, `help`,    `prints this help menu`                             },
+}
+
+// Usage string for wol
+var UsageString = `Usage:
+
+    <cyan>wol</cyan> [<options>] [mac address]
+
+Options:
+
+%s
+
+Specifying MAC addresses:
+-------------------------
+The following MAC addresses are valid and will match:
+01-23-45-56-67-89, 89:AB:CD:EF:00:12, 89:ab:cd:ef:00:12
+
+The supported delimiters include ":" and "-"
+
+Version:
+
+    <white>%s</white>
+
+`
+// Returns a tuple of commands and options which we support
+func getAllOptions() string {
+    options := ""
+    for _, o := range ValidOptions {
+        options += fmt.Sprintf("    <yellow>-%s --%-8s</yellow>    %s\n", o.short, o.long, o.description)
+    }
+    return options
+}
+
+// Returns the Usage string for this application
+func getAppUsageString() string {
+    options := getAllOptions()
+    return colorize.Colorize(fmt.Sprintf(UsageString, options, Version))
+}
+
--- wol/wol.go
+++ wol/wol.go
@@ -1,20 +1,33 @@
 package main
 
 import (
+    "os"
     "fmt"
+    "strings"
     "encoding/binary"
     "bytes"
     "net"
 
     wol "github.com/sabhiram/go-wol/lib"
+
+    "github.com/jessevdk/go-flags"
 )
 
-const macAddr = "08:BA:DF:00:00:0D"
-const udpPort = "9"
-const bcastAddr = "255.255.255.255"
+const (
+    UDPPort     = "9"
+    BcastAddr   = "255.255.255.255"
+)
 
-func main() {
+var (
+    // Define holders for the cli arguments we wish to parse
+    Options struct {
+        Version     bool   `short:"v" long:"version"`
+        Help        bool   `short:"h" long:"help"`
+    }
+)
 
+// Function to send a magic packet to a given mac address
+func sendMagicPacket(macAddr string) error {
     x, err := wol.NewMagicPacket(macAddr)
     if err != nil {
         fmt.Printf("Error: %s\n", err.Error())
@@ -23,23 +36,57 @@ func main() {
         var buf bytes.Buffer
         binary.Write(&buf, binary.BigEndian, x)
 
-        udpAddr, err := net.ResolveUDPAddr("udp", bcastAddr + ":" + udpPort)
+        udpAddr, err := net.ResolveUDPAddr("udp", BcastAddr + ":" + UDPPort)
         if err != nil {
-            fmt.Printf("Unable to get a UDP address for %s\n", bcastAddr)
-            return
+            fmt.Printf("Unable to get a UDP address for %s\n", BcastAddr)
+            return err
         }
 
         connection, err := net.DialUDP("udp", nil, udpAddr)
         if err != nil {
-            fmt.Printf("Unable to dial UDP addr for %s\n", bcastAddr)
-            return
+            fmt.Printf("Unable to dial UDP addr for %s\n", BcastAddr)
+            return err
         }
         defer connection.Close()
 
         bytesWritten, err := connection.Write(buf.Bytes())
-        fmt.Printf("% x\n", buf.Bytes())
         if bytesWritten != 102 {
             fmt.Printf("%d bytes written, %d expected!\n", bytesWritten, 102)
         }
     }
+    return err
+}
+
+func main() {
+    // Parse arguments which might get passed to "wol"
+    parser := flags.NewParser(&Options, flags.Default & ^flags.HelpFlag)
+    args, error := parser.Parse()
+    macAddr := strings.Join(args, " ")
+
+    exitCode := 0
+    switch {
+
+    // Parse Error, print usage
+    case error != nil:
+        fmt.Printf(getAppUsageString())
+        exitCode = 1
+
+    // No arguments, or help requested, print usage
+    case len(os.Args) == 1 || Options.Help:
+        fmt.Printf(getAppUsageString())
+
+    // "--version" requested
+    case Options.Version:
+        fmt.Printf("%s\n", Version)
+
+    // All other cases go here!
+    case true:
+        err := sendMagicPacket(macAddr)
+        if err != nil {
+            exitCode = 1
+        } else {
+            fmt.Printf("Magic packet sent successfully to %s\n", macAddr)
+        }
+    }
+    os.Exit(exitCode)
 }
