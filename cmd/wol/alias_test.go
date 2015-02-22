03c5aa729ee6fca897c4160803c0d1f2269612b8
Shaba Abhiram <shabarivas.abhiram@gmail.com>
2015-02-21 23:18:30 -0800

    decode to MacIface tests, wife is harassing me to stop working - more tomorrow

--- cmd/wol/alias_test.go
+++ cmd/wol/alias_test.go
@@ -1,11 +1,32 @@
 package main
 
 import (
+    "bytes"
+    "encoding/gob"
+
     "github.com/stretchr/testify/assert"
     "testing"
 )
 
 // This is just a dummy test to enable cross package coverage
 func TestDecodeToMacIface(test *testing.T) {
-    assert.Equal(test, 1, 1)
+    var TestCases = []MacIface {
+        MacIface{ "00:00:00:00:00:00", "eth0" },
+        MacIface{ "00:00:00:00:00:AA", "eth1" },
+    }
+
+    for _, entry := range TestCases {
+        // First encode the MacIface to a bunch of bytes
+        buf := bytes.NewBuffer(nil)
+        err := gob.NewEncoder(buf).Encode(entry);
+        assert.Equal(test, err, nil)
+
+        // Invoke the function and validate that it is equal
+        // to our starting MacIface
+        result, err := DecodeToMacIface(buf)
+        assert.Equal(test, err, nil)
+        assert.Equal(test, entry, result)
+    }
 }
+
+// TODO: Add BoltDB related alias tests here...
