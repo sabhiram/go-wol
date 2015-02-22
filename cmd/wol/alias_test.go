5fb01e21224eb57a91b8093eb22b685aab32aaa7
Shaba Abhiram <shabarivas.abhiram@gmail.com>
2015-02-21 22:42:23 -0800

    Enable testing of multiple packages

---
+++ cmd/wol/alias_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "github.com/stretchr/testify/assert"
+    "testing"
+)
+
+// This is just a dummy test to enable cross package coverage
+func TestDecodeToMacIface(test *testing.T) {
+    assert.Equal(test, 1, 1)
+}
